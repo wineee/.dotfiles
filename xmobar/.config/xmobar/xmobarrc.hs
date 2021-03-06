1bb03b54e6190437edf20996a0cd8564bc3261d5
wineee <1758075541@qq.com>
2021-03-07 00:47:26 +0800

    Set Vol Contorl

--- xmobar/.config/xmobar/xmobarrc.hs
+++ xmobar/.config/xmobar/xmobarrc.hs
@@ -1,24 +1,27 @@
 Config { 
-    font = "xft:UbuntuMono Nerd Font:weight=bold:pixelsize=16:antialias=true:hinting=true",
+    font = "xft:Noto Sans CJK Font:weight=bold:pixelsize=16:antialias=true:hinting=true",
     bgColor = "#292d3e",
     fgColor = "#f07178",
     lowerOnStart = True,
     hideOnStart = False,
     allDesktops = True,
-    persistent = True,
+    persistent = False,
+    overrideRedirect = True, 
+    position = Top,
+    iconRoot = "./xpm"
     alignSep = "}{",
     template = "<fc=#b303ff> λ </fc> %UnsafeStdinReader% }{ \
-        \<fc=#e1acff> %multicpu% | %coretemp% </fc>\
+        \<fc=#e1acff> %multicpu% </fc>\
         \<fc=#FFB86C> %dynnetwork% </fc>\
-        \<fc=#c3e88d> | %battery% </fc>\
-        \<fc=#81AAFF> | %memory% </fc>\
+        \<fc=#c3e88d> %battery% </fc>\
+        \<fc=#81AAFF> %memory% </fc> %default:Master%\
         \<fc=#8BE9FD> %date% </fc>"
     commands = [ 
-        Run Date "|  %d %b %Y %H:%M " "date" 600,
+        Run Date "%d %b %Y %H:%M " "date" 600,
         Run Com "bash" ["-c", "checkupdates | wc -l"] "updates" 3000,
         Run Com "~/.config/xmobar/trayer-padding-icon.sh" [] "trayerpad" 600,
-          -- battery monitor
-        Run Battery        [ "--template" , "Batt: <acstatus>"
+        Run Volume "default" "Master" [] 10
+        , Run Battery        [ "--template" , "Batt: <acstatus>"
                              , "--Low"      , "10"        -- units: %
                              , "--High"     , "80"        -- units: %
                              , "--low"      , "darkred"
@@ -33,6 +36,7 @@ Config {
                                        -- charged status
                                        , "-i"   , "<fc=#006000>Charged</fc>"
                              ] 50
+	
 	, Run DynNetwork     [ "--template" , "<dev>: <tx>kB/s|<rx>kB/s"
                              , "--Low"      , "1000"       -- units: B/s
                              , "--High"     , "5000"       -- units: B/s
@@ -40,6 +44,7 @@ Config {
                              , "--normal"   , "darkorange"
                              , "--high"     , "darkred"
                              ] 10
+	
 	, Run Memory         [ "--template" ,"Mem: <usedratio>%"
                              , "--Low"      , "20"        -- units: %
                              , "--High"     , "90"        -- units: %
@@ -47,6 +52,7 @@ Config {
                              , "--normal"   , "darkorange"
                              , "--high"     , "darkred"
                              ] 10
+	
 	, Run MultiCpu       [ "--template" , "Cpu: <total0>%|<total1>%"
                              , "--Low"      , "50"         -- units: %
                              , "--High"     , "85"         -- units: %
@@ -55,14 +61,15 @@ Config {
                              , "--high"     , "darkred"
                              ] 10
 
-        -- cpu core temperature monitor
-        , Run CoreTemp       [ "--template" , "Temp: <core0>°C|<core1>°C"
+        
+	, Run CoreTemp       [ "--template" , "Temp: <core0>°C|<core1>°C"
                              , "--Low"      , "70"        -- units: °C
                              , "--High"     , "80"        -- units: °C
                              , "--low"      , "darkgreen"
                              , "--normal"   , "darkorange"
                              , "--high"     , "darkred"
                              ] 50
+	
 	, Run UnsafeStdinReader
     ],
 }
